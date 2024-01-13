f3c39c6d8b4553256ecc450b0d1520457b7677a1
Nis-Nissy-Bhatt <145188524+Nis-Nissy-Bhatt@users.noreply.github.com>
2024-01-13 22:28:15 +0530

    Update APITest.java

--- src/test/java/mavenforjenkins/APITest.java
+++ src/test/java/mavenforjenkins/APITest.java
@@ -5,7 +5,7 @@ public class APITest {
 	public static void main(String args[])
 	{
 		System.out.println("Code run from JAVA");
-		System.out.println("Any thing can be done in JAVA here is GIT rep ends");
+		System.out.println("Any thing can be done in JAVA here is GIT repo ends");
 	}
 	
 }
