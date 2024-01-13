2dcbc11582cfadffeaa22afb744c90db4e29c1d3
Nis-Nissy-Bhatt <145188524+Nis-Nissy-Bhatt@users.noreply.github.com>
2024-01-13 22:42:08 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class APITest {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Code run from JAVA");
+		System.out.println("Any thing can be done in JAVA here is GIT repo ends");
+	}
+	
+}
